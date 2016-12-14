f905fad7ee4c34405a4cc9f1cc54f3ddea3cce13
Zarin Bhuiyan <zarin.bhuiyan@students.olin.edu>
2016-12-13 21:09:18 -0500

    Added things.

--- Clock.java
+++ Clock.java
@@ -23,6 +23,17 @@ public class Clock {
 
 
 	/**
+	* This method checks to see whether the clock is paused and then
+	* sets the static variable as so.
+	*
+	* @param boolean isPaused, if the clock is paused.
+	*/
+	public void pause(boolean isPaused){
+		this.isPaused = isPaused;
+	}
+
+
+	/**
 	* This method gets the current time of the game. It calculates this
 	* in milliseconds and is very precise and quick.
 	*
@@ -39,7 +50,7 @@ public class Clock {
 	* @param float cyclesPerSec This is the number of cycles that pass each second.
 	*/
 	public void setCyclesPerSec(float cyclesPerSec){
-		this.msPerCycle = (1f/cyclesPerSec)*1000; //Number of cycles per cycle. 1f specifies 1 is a float
+		this.msPerCycle = (1f/cyclesPerSec)*1000; //Number of cycles per sec. 1f specifies 1 is a float
 	}
 
 
@@ -48,8 +59,54 @@ public class Clock {
 	*
 	*/
 	public void updateClock(){
-		// float delta = (float)(getCurrentTime() - 
+		// The delta time calculated from subtracting last clock update from
+		// the current time.
+		float delta = (float)(this.getCurrentTime() - lastClockUpdate); 
+		while(!isPaused){
+			// Number of passed cycles is calculated by flooring the value
+			// of delta divided by the number of milliseconds per cycle.
+			this.numPassedCycles += (int)Math.floor(delta / msPerCycle);
+		}
+		this.lastClockUpdate = getCurrentTime();
 	}
+
+	/**
+	* This method resets the clock.
+	*
+	*/
+	public void reset(){
+		this.isPaused = false;
+		this.numPassedCycles = 0;
+		this.lastClockUpdate = this.getCurrentTime();
+	}
+
+	/**
+	* This method returns whether the clock is paused.
+	*
+	* @return boolean if the clock is paused.
+	*/
+	public boolean isPaused(){
+		return this.isPaused;
+	}
+
+	/**
+	* This method checks to see if the number of passed cycles is
+	* greater than 0. If so, it decrements each time and returns true.
+	* If not, it returns false.
+	*
+	* @return boolean if the number of passed cycles is greater or less than 0.
+	*/
+	public boolean hasPassedCycle(){
+		if(numPassedCycles > 0){
+			this.numPassedCycles--; //If the number of passed cycles is greater than 0, it will decrement		
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+
+
 
 
 }
